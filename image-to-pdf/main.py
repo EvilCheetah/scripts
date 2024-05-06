dc8f2cc0377a630a9bcad9b8653c9570794f6607
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-05-05 17:57:24 -0700

    add(): argument parsing to specify target for script execution

--- image-to-pdf/main.py
+++ image-to-pdf/main.py
@@ -1,10 +1,26 @@
-def main():
-    pass
+import argparse
+from pathlib import Path
+
+
+def main(directory = Path('.')):
+    print(f'Passed Directory: `{directory}`')
 
 
 if __name__ == '__main__':
     try:
-        main()
+        parser = argparse.ArgumentParser(
+            description = 'Converts all images in folder to PDF'
+        )
+        
+        parser.add_argument(
+            '--directory', '--dir', '-d',
+            help    = 'Directory Path',
+            default = Path('.')
+        )
+        
+        args = parser.parse_args()
+        
+        main(directory = args.directory)
 
     except KeyboardInterrupt:
         print('Terminated...')
