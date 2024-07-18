ebd851e7e29c467aa5902aa5cab8fd5bc96db966
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-07-18 15:21:31 -0700

    add(): prototype logic

--- relay-payments/main.py
+++ relay-payments/main.py
@@ -1,5 +1,39 @@
+import pandas as pd
+
+columns = [
+    'InvoiceNumber',
+    'CalendarWeek',
+    'ContractID',
+    'BlockID',
+    'TripID',
+    'LoadID',
+    'StartDate',
+    'EndDate',
+    'RunDomicile',
+    'OperatorType',
+    'Equipment',
+    'Distance',
+    'ItemType',
+    'ProgramType',
+    'BaseRate',
+    'FuelSurcharge',
+    'Tolls',
+    'Detention',
+    'TONU',
+    'Others',
+    'GrossPay',
+    'Comments',
+]
+
+
 def main():
-    pass
+    file = pd.ExcelFile('Week 28 - July 7 — July 13 - Contract.xlsx')
+    df   = pd.read_excel(file, sheet_name = 'Payment Details', names = columns)
+
+    df.columns = columns
+
+    for row in df.itertuples(index = False, name = 'PaymentEntry'):
+        print(f'Block: {row.BlockID} - ${row.BaseRate}')
 
 
 if __name__ == '__main__':
