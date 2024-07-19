169f69673aa7d7744ded75f1867f0bf9e83b82e9
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-07-18 22:37:44 -0700

    add(): implementation

--- relay-payments/main.py
+++ relay-payments/main.py
@@ -1,39 +1,32 @@
+from collections import defaultdict
+from decimal import Decimal
+from functools import reduce
+
 import pandas as pd
+import numpy as np
 
-columns = [
-    'InvoiceNumber',
-    'CalendarWeek',
-    'ContractID',
-    'BlockID',
-    'TripID',
-    'LoadID',
-    'StartDate',
-    'EndDate',
-    'RunDomicile',
-    'OperatorType',
-    'Equipment',
-    'Distance',
-    'ItemType',
-    'ProgramType',
-    'BaseRate',
-    'FuelSurcharge',
-    'Tolls',
-    'Detention',
-    'TONU',
-    'Others',
-    'GrossPay',
-    'Comments',
-]
+from columns import COLUMNS
 
 
-def main():
+TRIPS = defaultdict(lambda: Decimal(0))
+
+
+def main() -> None:
     file = pd.ExcelFile('Week 28 - July 7 — July 13 - Contract.xlsx')
-    df   = pd.read_excel(file, sheet_name = 'Payment Details', names = columns)
-
-    df.columns = columns
+    df   = pd.read_excel(file, sheet_name = 'Payment Details', names = COLUMNS)
+    df   = df.replace({ np.nan: None })
 
     for row in df.itertuples(index = False, name = 'PaymentEntry'):
-        print(f'Block: {row.BlockID} - ${row.BaseRate}')
+        TRIPS[row.BlockID or row.TripID or row.LoadID] += Decimal(row.GrossPay or 0)
+
+    # Remove rows where the Summary column contains None (originally NaN) 
+    TRIPS.pop(None)
+
+    for trip_id, pay_amount in TRIPS.items():
+        print(f'Trip: {trip_id} - ${pay_amount:,.2f}')
+
+    total = reduce(lambda a, b: a + b, TRIPS.values())
+    print(f'\tTotal: ${total:,.2f}')
 
 
 if __name__ == '__main__':
