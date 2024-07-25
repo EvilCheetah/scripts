d5b6aa51977613b994cc836c06043061e7914883
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-07-24 21:09:19 -0700

    refactor(): to have contract and spot columns; read desc each report has different columns, a try to parse using the same column list leads to a runtime error

--- relay-payments/columns.py
+++ relay-payments/columns.py
@@ -1,4 +1,4 @@
-COLUMNS = [
+CONTRACT_COLUMNS = [
     'InvoiceNumber',
     'CalendarWeek',
     'ContractID',
@@ -22,3 +22,33 @@ COLUMNS = [
     'GrossPay',
     'Comments',
 ]
+
+
+SPOT_COLUMNS = [
+    'InvoiceNumber',
+    'BlockID',
+    'TripID',
+    'LoadID',
+    'StartDate',
+    'EndDate',
+    'RunDomicile',
+    'OperatorType',
+    'Equipment',
+    'Distance',
+    'ItemType',
+    'ProgramType',
+    'BaseRate',
+    'FuelSurcharge',
+    'Tolls',
+    'Detention',
+    'TONU',
+    'Others',
+    'GrossPay',
+    'Comments',
+]
+
+
+COLUMNS = {
+    'Contract': CONTRACT_COLUMNS,
+    'Spot':     SPOT_COLUMNS
+}
